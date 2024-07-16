c3afbed5aec0ecc2af1dcf16a0617e589234971a
Pupunha Dev <pupunha.dev@gmail.com>
2024-07-16 16:57:19 -0300

    Prisma installed. DB Schema created

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 
 export default function Home() {
+
   return (
-    <div>
-      <h1>Auth JS</h1>
+    <div className="flex flex-col p-2">
+      {/* https://www.youtube.com/watch?v=RveI1EjXUX0 52:37 */}
+      <button className="bg-zinc-500 font-bold text-white p-2 rounded-md w-[200px]">Login Simples</button>
     </div>
   );
 }
